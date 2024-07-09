4bdfc65930b5b282fd68c73996b85f173330bbd2
Nigam Niraula <nigam21nir@gmail.com>
2024-07-09 17:47:26 +0545

    Add New Projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -6,6 +6,9 @@ import { Parallax, ParallaxProvider } from "react-scroll-parallax";
 import prabigya from "../../Assets/Projects/Prabigya.png";
 import autoffic from "../../Assets/Projects/autoffic.svg";
 import prophesy from "../../Assets/Projects/battery.png";
+import swiftCart from "../../Assets/Projects/SwiftCart.png";
+import FarmApp from "../../Assets/Projects/FarmApp.png";
+import Tetris from "../../Assets/Projects/Play Tetris.png";
 function Projects() {
   return (
     <Container fluid className="project-section" id="project">
@@ -31,6 +34,17 @@ function Projects() {
               ghLink="https://github.com/orgs/Prabigyaa/repositories"
             />
           </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={swiftCart}
+              isBlog={false}
+              title="SwiftCart"
+              description="A simple e-commerce application that provides a platform for vendors to list their products, customers to browse
+and purchase items, and facilitate reviews for products utilizing MERN.
+"
+              ghLink="https://github.com/ninix07/SwiftCart"
+            />
+          </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
@@ -43,11 +57,34 @@ function Projects() {
           </Col>
           <Col md={4} className="project-card">
             <ProjectCard
+              imgPath={FarmApp}
+              isBlog={false}
+              title="Farm App"
+              description="Farm App is a inventory management app made using React Native for chicken farms to manage eggs, chicken and
+chicken's feed, water, light, etc.
+
+"
+              ghLink="https://github.com/KALPREX-ai/kukhuri_farm_frontend"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
               imgPath={prophesy}
               isBlog={false}
               title="Prophesy"
               description="Prophesy is a battery monitoring software tailored for linux and windows user. It focuses on providing relevant information about battery and using it to predict the remaining time of use for given charge cycle using neural network and regression algorithms."
               ghLink="https://github.com/earthPerson-001/prophesy"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={Tetris}
+              isBlog={false}
+              title="PlayTetris"
+              description="PlayTetris is a C++ and QT based project that implements a playable version of the classic Tetris game, featuring a
+graphical user interface for user interaction and game controls.
+"
+              ghLink="https://github.com/ninix07/PlayTetris"
             />
           </Col>
         </Row>
